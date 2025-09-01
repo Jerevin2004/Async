05fb406eec0699b27366e6035aeccf6c47eb9f42
Jerevin2004 <rodelasjerevin15@gmail.com>
2025-09-01 14:13:54 +0800

    Add files via upload

---
+++ Async1/script.js
@@ -0,0 +1,37 @@
+//part 1
+
+concole.log("First message");
+
+setTimeout(()=> {
+    console.log ("Second message (after 2)");
+}, 2000);
+
+console.log("Third message");
+
+
+//part 2
+
+fetch("https://api.adviceslip.com/advice")
+   .then(response => response.json())
+   .then(data => {
+       console.log("Here's your advice:",data.slip.advice);
+   })
+   .catch(error => console.error("Error;", error));
+
+
+//part 3
+
+
+async function getAdvice() {
+    try{
+        let response = await fetch("https://api.adviceslip.com/advice");
+        let data = await response.json();
+        console.log("Here's your advice:", data.slip.advice);
+    } catch (error) {
+        console.error("Error:", error)
+
+    }
+}
+
+
+
